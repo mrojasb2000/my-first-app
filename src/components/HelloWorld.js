f542f6978a03a9f818dea53cf78c78b6c458d1fa
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-26 21:59:46 -0300

    refactor: formatted string

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 export default function HelloWorld(props) {
   const strFormatted = (props) => {
-    const name = props.name
     const age = props.age ? ' your age is ' + props.age : ''
-    return 'HelloWorld ' + name + age
+    return 'HelloWorld ' + props.name + age
   }
   return <div>{strFormatted(props)}</div>
 }
