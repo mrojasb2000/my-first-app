99ddd23b1ac4f449935f270085bf1e4a0fdd6110
Mauricio Rojas <mrojasb2000@gmail.com>
2022-11-29 12:19:50 -0300

    chore: added destructuring params

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function HelloWorld(props) {
+  const { user, fn } = props
+
   const strFormatted = (user) => {
     const age = user.age ? ' your age is ' + user.age : ''
     return 'HelloWorld ' + user.name + age
@@ -8,8 +10,8 @@ export default function HelloWorld(props) {
 
   return (
     <div>
-      {strFormatted(props.user)}
-      <button onClick={() => props.fn(props.user.name)}>Hello World</button>
+      {strFormatted(user)}
+      <button onClick={() => fn(user.name)}>Hello World</button>
     </div>
   )
 }
